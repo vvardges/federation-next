320ef4246b77b27138b6d0d04623a638ab217b8c
Vardges Vardanyan <vako3008@gmail.com>
2020-05-21 15:24:17 +0400

    create custom server

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.prepare().then(() => {
         } else {
             handle(req, res, parsedUrl);
         }
-    }).listen(8080, err => {
+    }).listen(8000, err => {
         if (err) throw err;
-        console.log('> Ready on http://localhost:8080');
+        console.log('> Ready on http://localhost:8000');
     })
 });
