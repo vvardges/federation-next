14c881b387309f44998b4860fe8ae4127f5c447d
Vardges Vardanyan <vako3008@gmail.com>
2020-06-08 17:41:41 +0400

    set favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
         return (
             <html>
             <Head>
+                <link rel="icon" type="image/svg" href="/img/favicon.svg" sizes="32x32"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <meta name="yandex-verification" content="90821fb86ed59eeb" />
             </Head>
