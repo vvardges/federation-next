6d3456cb1c7737cacd109e0c40513287b2144ac2
Vardges Vardanyan <vako3008@gmail.com>
2020-06-23 13:13:31 +0400

    fix subheading name in category

--- components/posts/small.js
+++ components/posts/small.js
@@ -4,10 +4,10 @@ import Link from "next/link";
 const Small = ({ posts }) => (
     <div className="bg-secondary border-black border-left border-md mb-3 py-2">
         <div className="card-group flex-column flex-lg-row">
-            {posts.map((post, index) =>
-                <div className="card bg-secondary" key={index}>
+            {posts.map(post =>
+                <div className="card bg-secondary" key={post.id}>
                     <div className="card-header">
-                        <small className="font-family-condensed letter-spacing-lg">{post.source ? post.source.name : ""}</small>
+                        <small className="font-family-condensed letter-spacing-lg">{post.sub_heading.title}</small>
                     </div>
                     <div className="card-body py-0">
                         <Link href="/post/[slug]" as={`/post/${post.slug}`}>
@@ -16,7 +16,7 @@ const Small = ({ posts }) => (
                     </div>
                     <div className="card-footer d-flex no-gutters">
                         <div className="col-auto">
-                            <small className="text-muted font-family-condensed">{post.publishedAt}</small>
+                            <small className="text-muted font-family-condensed">{post.published_diff_for_humans}</small>
                         </div>
                         <div className="col d-lg-none">
                             <hr className="m-2 border-light-gray" />
