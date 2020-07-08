9f9b99cdeeef25cc22161f3875b0e03ca4a98ea1
Vardges Vardanyan <vako3008@gmail.com>
2020-07-08 23:51:39 +0400

    chart corrections

--- components/content.js
+++ components/content.js
@@ -9,37 +9,44 @@ import {
     HorizontalGridLines,
     VerticalBarSeries,
     HorizontalBarSeries,
-    RadialChart, ChartLabel, LineSeries
+    RadialChart, ChartLabel, LineMarkSeries
 } from 'react-vis';
 import Gallery from "./gallery";
 
+const axisStyle = {
+    line: {stroke: '#B6B6B6'},
+    ticks: {stroke: '#B6B6B6'},
+    text: {stroke: 'none', fill: '#757575'}
+};
+
+const colors = ['#4F91CF', '#99C1E5', '#FFA24B', '#3DB45E', '#F56A4C', '#A6CF4F', '#D48FEC'];
+
 function ChartGenerator() {
     return (
-        <div>
-            <XYPlot xType="ordinal" width={300} height={300} xDistance={100}>
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
-                <VerticalBarSeries data={data} />
+        <div className="font-family-condensed">
+            <XYPlot xType="ordinal" width={570} height={360} >
+                <XAxis style={axisStyle}/>
+                <YAxis style={axisStyle}/>
+                <VerticalBarSeries data={[{x: 2016, y: 1}, {x: 2017, y: 2}, {x: 2018, y: 12}, {x: 2019, y: 15}]} color="#4F91CF"/>
             </XYPlot>
-            <XYPlot width={300} height={300} stackBy="x" yType="ordinal">
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
-                <HorizontalBarSeries data={[{y: 'Am', x: 10}, {y: 'Uk', x: 5}, {y: 'Ru', x: 15}]} color="#000"/>
-                <HorizontalBarSeries data={[{y: 'Am', x: 12}, {y: 'Uk', x: 2}, {y: 'Ru', x: 11}]} />
+            <XYPlot width={660} height={360} stackBy="x" yType="ordinal" margin={{left: 100}}>
+                <YAxis style={axisStyle} tickSize={1}/>
+                <HorizontalBarSeries data={[{y: 'Poland', x: 10}, {y: 'Bulgaria', x: 5}, {y: 'Romania', x: 15}, {y: 'Latvia', x: 10}, {y: 'Turkey', x: 5}, {y: 'Germany', x: 15}]} color={colors[0]}/>
+                <HorizontalBarSeries data={[{y: 'Poland', x: 1}, {y: 'Bulgaria', x: 15}, {y: 'Romania', x: 22}, {y: 'Latvia', x: 2}, {y: 'Turkey', x: 3}, {y: 'Germany', x: 4}]} color={colors[1]}/>
             </XYPlot>
-            <RadialChart width={300} height={300} data={[{angle: 45}, {angle: 55}]}/>
-            <XYPlot width={300} height={300}>
+            <RadialChart
+                width={300}
+                height={300}
+                data={[{angle: 45, color: colors[0]}, {angle: 45, color: colors[1]}, {angle: 5, color: colors[2]}, {angle: 10, color: colors[3]}]}
+            />
+            <XYPlot width={570} height={500}>
                 <HorizontalGridLines />
-                <VerticalGridLines />
-                <XAxis />
-                <YAxis />
-                <LineSeries
-                    className="first-series"
-                    data={[{x: 1, y: 1}, {x: 2, y: 2}, {x: 3, y: 12}, {x: 4, y: 15}]}
+                <XAxis style={axisStyle}/>
+                <YAxis style={axisStyle}/>
+                <LineMarkSeries
+                    data={[{x: 1, y: 5.5}, {x: 2, y: 3}, {x: 3, y: 7}, {x: 3, y: 4}, {x: 5, y: 3}]}
+                    lineStyle={{fill:"none", stroke:"#CDE7FF", strokeWidth: 4}}
+                    markStyle={{fill:"#4F91CF"}}
                 />
             </XYPlot>
         </div>
