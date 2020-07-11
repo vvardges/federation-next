8aebf6b6dea9887662e7950819fb734b2683d642
Vardges Vardanyan <vako3008@gmail.com>
2020-07-11 15:08:02 +0400

    connect charts with API

--- components/content.js
+++ components/content.js
@@ -5,11 +5,10 @@ import {
     XYPlot,
     XAxis,
     YAxis,
-    VerticalGridLines,
     HorizontalGridLines,
     VerticalBarSeries,
     HorizontalBarSeries,
-    RadialChart, ChartLabel, LineMarkSeries
+    RadialChart, LineMarkSeries
 } from 'react-vis';
 import Gallery from "./gallery";
 
@@ -21,36 +20,53 @@ const axisStyle = {
 
 const colors = ['#4F91CF', '#99C1E5', '#FFA24B', '#3DB45E', '#F56A4C', '#A6CF4F', '#D48FEC'];
 
-function ChartGenerator() {
-    return (
-        <div className="font-family-condensed">
-            <XYPlot xType="ordinal" width={570} height={360} >
-                <XAxis style={axisStyle}/>
-                <YAxis style={axisStyle}/>
-                <VerticalBarSeries data={[{x: 2016, y: 1}, {x: 2017, y: 2}, {x: 2018, y: 12}, {x: 2019, y: 15}]} color="#4F91CF"/>
-            </XYPlot>
-            <XYPlot width={660} height={360} stackBy="x" yType="ordinal" margin={{left: 100}}>
-                <YAxis style={axisStyle} tickSize={1}/>
-                <HorizontalBarSeries data={[{y: 'Poland', x: 10}, {y: 'Bulgaria', x: 5}, {y: 'Romania', x: 15}, {y: 'Latvia', x: 10}, {y: 'Turkey', x: 5}, {y: 'Germany', x: 15}]} color={colors[0]}/>
-                <HorizontalBarSeries data={[{y: 'Poland', x: 1}, {y: 'Bulgaria', x: 15}, {y: 'Romania', x: 22}, {y: 'Latvia', x: 2}, {y: 'Turkey', x: 3}, {y: 'Germany', x: 4}]} color={colors[1]}/>
-            </XYPlot>
-            <RadialChart
-                width={300}
-                height={300}
-                data={[{angle: 45, color: colors[0]}, {angle: 45, color: colors[1]}, {angle: 5, color: colors[2]}, {angle: 10, color: colors[3]}]}
-            />
-            <XYPlot width={570} height={500}>
-                <HorizontalGridLines />
-                <XAxis style={axisStyle}/>
-                <YAxis style={axisStyle}/>
-                <LineMarkSeries
-                    data={[{x: 1, y: 5.5}, {x: 2, y: 3}, {x: 3, y: 7}, {x: 3, y: 4}, {x: 5, y: 3}]}
-                    lineStyle={{fill:"none", stroke:"#CDE7FF", strokeWidth: 4}}
-                    markStyle={{fill:"#4F91CF"}}
+function ChartGenerator({chartType, data}) {
+    switch (chartType) {
+        case "VerticalBar":
+            return (
+                <XYPlot xType="ordinal" width={570} height={360} >
+                    <XAxis style={axisStyle}/>
+                    <YAxis style={axisStyle}/>
+                    <VerticalBarSeries data={data} color="#4F91CF"/>
+                </XYPlot>
+            );
+        case "HorizontalBar":
+            return (
+                <XYPlot width={660} height={360} stackBy="x" yType="ordinal" margin={{left: 100}}>
+                    <YAxis style={axisStyle} tickSize={1}/>
+                    {data.map((d, i) =>
+                        <HorizontalBarSeries key={i} data={d.values.map(v => {return {x:+v.x, y:v.y}})} color={colors[i]}/>
+                    )}
+                </XYPlot>
+            );
+        case "Radial":
+            return (
+                <RadialChart
+                    colorType="literal"
+                    width={300}
+                    height={300}
+                    data={data.map((d, i) => {
+                        return {
+                            angle: +d.label,
+                            color: colors[i]
+                        }
+                    })}
                 />
-            </XYPlot>
-        </div>
-    )
+            );
+        case "Line":
+            return (
+                <XYPlot width={570} height={500} xType="ordinal" yType="ordinal">
+                    <HorizontalGridLines />
+                    <XAxis style={axisStyle}/>
+                    <YAxis style={axisStyle}/>
+                    <LineMarkSeries
+                        data={data}
+                        lineStyle={{fill:"none", stroke:"#CDE7FF", strokeWidth: 4}}
+                        markStyle={{fill:"#4F91CF"}}
+                    />
+                </XYPlot>
+            );
+    }
 }
 
 function ContentGenerator({ data }) {
@@ -115,14 +131,17 @@ function ContentGenerator({ data }) {
                     />
                 </div>
             );
-        default:
+        case "chart":
             return (
                 <div className="d-flex justify-content-center">
-                    <div className="d-inline-flex bg-secondary">
-                        <ChartGenerator/>
+                    <div className="bg-secondary border-top border-md p-3 font-family-condensed">
+                        <p className="letter-spacing-lg">{data.title}</p>
+                        <ChartGenerator {...data}/>
                     </div>
                 </div>
             );
+        default:
+            return null;
     }
 }
 
