7df43a13db77146978b36f246238539dc034731b
Vardges Vardanyan <vako3008@gmail.com>
2020-07-23 22:59:25 +0400

    add social icons in the footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,14 @@
 import Link from "next/link";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {getSettings} from "../lib/categories";
 
 export default function Footer() {
+    const [settings, setSettings] = useState([]);
+
+    useEffect( () => {
+        getSettings().then(data => setSettings(data));
+    }, []);
+
     return (
         <div className="border-top border-black bg-secondary text-center font-family-condensed py-1">
             <div className="container smaller">
@@ -11,12 +18,14 @@ export default function Footer() {
                     </a>
                 </Link>
                 <hr/>
-                <div className="mb-4">
-                    <a href="#" className="badge rounded-circle bg-white text-black-50 py-1 mx-1"><i className="icon-facebook lead"/></a>
-                    <a href="#" className="badge rounded-circle bg-white text-black-50 py-1 mx-1"><i className="icon-telegram lead"/></a>
-                    <a href="#" className="badge rounded-circle bg-white text-black-50 py-1 mx-1"><i className="icon-twitter lead"/></a>
-                    <a href="#" className="badge rounded-circle bg-white text-black-50 py-1 mx-1"><i className="icon-vk lead"/></a>
-                </div>
+
+                {settings.length && <div className="mb-4">
+                    <a href={settings.find(s => s.name === "Facebook_link").value} target="_blank" className="badge rounded-circle bg-white text-black-50 py-1 mx-1"><i className="icon-facebook lead"/></a>
+                    <a href={settings.find(s => s.name === "Telegram_link").value} target="_blank" className="badge rounded-circle bg-white text-black-50 py-1 mx-1"><i className="icon-telegram lead"/></a>
+                    <a href={settings.find(s => s.name === "Twitter_link").value} target="_blank" className="badge rounded-circle bg-white text-black-50 py-1 mx-1"><i className="icon-twitter lead"/></a>
+                    <a href={settings.find(s => s.name === "VK_link").value} target="_blank" className="badge rounded-circle bg-white text-black-50 py-1 mx-1"><i className="icon-vk lead"/></a>
+                </div>}
+
                 <p className="text-muted mb-3 letter-spacing-lg">Полное или частное использование материалов возможно только с разрешением редакции.</p>
                 <div className="letter-spacing-lg my-4">
                     <Link href="/page/[slug]" as="/page/privacy-policy">
