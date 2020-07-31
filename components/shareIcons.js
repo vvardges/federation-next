517a48defb58ef418f7df419616ae82b5fd22f6d
Vardges Vardanyan <vako3008@gmail.com>
2020-07-31 12:07:34 +0400

    corrections

--- components/shareIcons.js
+++ components/shareIcons.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ShareIcons = ({ fullPath }) => {
     return (
-        <div>
+        <div className="d-flex flex-nowrap">
             <a href={`https://www.facebook.com/sharer/sharer.php?u=${fullPath}`} className="badge rounded-circle bg-secondary text-black-50 py-1 mr-1" target="_blank">
                 <i className="icon-facebook lead"/>
             </a>
