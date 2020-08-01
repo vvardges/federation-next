ec728998097b4d585ed4c2c39b3a7ce148afe16e
Vardges Vardanyan <vako3008@gmail.com>
2020-08-01 15:19:44 +0400

    fix button position in 404

--- pages/404.js
+++ pages/404.js
@@ -6,17 +6,19 @@ export default function NotFound () {
     return (
         <div className="d-flex flex-column min-vh-100">
             <Header/>
-            <div className="pt-4 bg-dark col">
-                <div className="text-gray col-lg-6 mx-auto">
+            <div className="pt-4 bg-dark col-auto">
+                <div className="text-gray col-lg-6 mx-auto position-relative">
                     <h1>Упссс!</h1>
                     <hr className="border-gray"/>
                     <p className="lead letter-spacing-sm mb-5">Мы не можем найти страницу, которую ты ищешь</p>
                     <img src="/img/404.png" className="w-100" alt=""/>
-                    <div className="text-right mt-3">
-                        <Link href="/">
-                            <a className="btn btn-link btn-lg text-white font-family-condensed letter-spacing-lg">На главную <i className="icon-arrow-right h6 ml-2"/></a>
-                        </Link>
-                    </div>
+                </div>
+            </div>
+            <div className="col bg-dark align-items-center d-flex">
+                <div className="text-right col-lg-6 mx-auto">
+                    <Link href="/">
+                        <a className="btn btn-link btn-lg text-white font-family-condensed letter-spacing-lg">На главную <i className="icon-arrow-right h6 ml-2"/></a>
+                    </Link>
                 </div>
             </div>
         </div>
