26c33bba5f5b4f17ab42e1eb37d756e7baf0ede8
Vardges Vardanyan <vako3008@gmail.com>
2020-08-06 20:59:57 +0400

    add not found images

--- components/image.js
+++ components/image.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 
 const Image = ({ post, size }) => {
+    const [hasError, setHasError] = useState(false);
     return (
         <Link href="/post/[slug]" as={`/post/${post.slug}`}>
-            <a className="d-inline-block"><img src={post[`img_${size}`]} className={`img-fluid`} alt="..."/></a>
+            <a className="d-inline-block">
+                {hasError ?
+                    <img src={`/img/NF${size}.png`} className="img-fluid"/> :
+                    <img
+                        src={post[`img_${size}`]}
+                        className={`img-fluid`}
+                        onError={() => setHasError(true)}
+                        alt="..."
+                    />
+                }
+            </a>
         </Link>
     )
 };
