1cc7b73cc41ec52df741165e0d41db35286b366c
Vardges Vardanyan <vako3008@gmail.com>
2020-08-07 15:58:57 +0400

    add dots in pagination

--- components/pagination.js
+++ components/pagination.js
@@ -2,14 +2,16 @@ import React from "react";
 import {useRouter} from "next/router";
 
 export default function Pagination ({ totalPages }) {
-    const pages = [];
-    for (let i = 1; i <= totalPages; i++) {
-        pages.push(i);
-    }
-
     const router = useRouter();
     const currentPage = +router.query.page;
 
+    let pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+        pages.push(i);
+    }
+    pages.splice(1, currentPage - 2);
+    pages.splice(currentPage + 1, totalPages - currentPage - 2);
+    if (pages[pages.length - 1] !== totalPages) pages.push(totalPages);
 
     const handleClick = (page) => {
         const query = {...router.query, page};
@@ -29,11 +31,22 @@ export default function Pagination ({ totalPages }) {
                         <i className="icon-arrow-left"/>
                     </a>
                 </li>}
-                {pages.map(page =>
-                    <li key={page} className={`page-item mx-1 ${currentPage === page ? "active" : "cursor-pointer"}`} onClick={() => handleClick(page)}>
-                        <a className="page-link rounded-circle">{page}</a>
-                    </li>
-                )}
+                {pages.map((page, index) => {
+                    return (index > 0 && page !== pages[index-1] + 1) ? (
+                        <>
+                            <li key={page + "dots"} className={`page-item mx-1`}>
+                                ...
+                            </li>
+                            <li className={`page-item mx-1 ${currentPage === page ? "active" : "cursor-pointer"}`} onClick={() => handleClick(page)}>
+                                <a className="page-link rounded-circle">{page}</a>
+                            </li>
+                        </>
+                    ) : (
+                        <li key={page} className={`page-item mx-1 ${currentPage === page ? "active" : "cursor-pointer"}`} onClick={() => handleClick(page)}>
+                            <a className="page-link rounded-circle">{page}</a>
+                        </li>
+                    )
+                })}
                 {totalPages > 1 && <li className={`page-item mr-2 ${currentPage === totalPages ? "disabled" : "cursor-pointer"}`} onClick={() => handleClick(Math.min(currentPage + 1, totalPages))}>
                     <a className="page-link rounded-circle">
                         <i className="icon-arrow-right"/>
